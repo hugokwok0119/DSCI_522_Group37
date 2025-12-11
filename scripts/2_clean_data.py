55f50798270f032b838401d75bc273d2c68f2138
JayLBean <lyusheng0606@gmail.com>
2025-12-10 16:33:50 -0800

    Update scaled scripts

--- scripts/2_clean_data.py
+++ scripts/2_clean_data.py
@@ -2,6 +2,9 @@ import os
 import click
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.compose import make_column_transformer, make_column_selector
+from sklearn.preprocessing import StandardScaler
+import pickle
 
 @click.command()
 @click.option('--input-file', '-i',
@@ -74,16 +77,55 @@ def main(input_file, output_file):
         df.to_csv(output_file, index=False)
         click.echo(click.style(f"Successfully saved processed data to {output_file}", fg='green'))
         
-        
-        
         clean_train_df, clean_test_df = train_test_split(
             df, test_size=0.2, random_state=123, stratify=df['Diagnosis']
         )
         clean_train_df.to_csv('data/processed/clean_train.csv', index=False)
         clean_test_df.to_csv('data/processed/clean_test.csv', index=False)
         
-        click.echo(click.style(f"Successfully saved split processed data to {output_file}", fg='green'))
+        click.echo(click.style(f"Successfully saved split processed data", fg='green'))
         
+        numeric_feats = [
+            'radius_mean', 'texture_mean', 'smoothness_mean', 'compactness_mean',
+            'concavity_mean', 'concave_points_mean', 'symmetry_mean', 'fractal_dimension_mean',
+            'radius_se', 'texture_se', 'smoothness_se', 'compactness_se', 'concavity_se',
+            'concave_points_se', 'symmetry_se', 'fractal_dimension_se',
+            'radius_max', 'texture_max', 'smoothness_max', 'compactness_max',
+            'concavity_max', 'concave_points_max', 'symmetry_max', 'fractal_dimension_max'
+        ]
+
+        drop_feats = [
+            'perimeter_mean', 'area_mean',
+            'perimeter_se', 'area_se',
+            'texture_se', 'smoothness_se', 'symmetry_se',
+            'perimeter_max', 'area_max'
+        ]
+
+        ct = make_column_transformer(
+            (StandardScaler(), numeric_feats),
+            ("drop", drop_feats),
+            remainder="passthrough" 
+        )
+        
+        os.makedirs("results/models", exist_ok=True)
+        pickle.dump(ct, open("results/models/ct.pickle", "wb"))
+
+        ct.fit(clean_train_df)
+        scaled_clean_test = ct.transform(clean_test_df)
+        scaled_clean_train = ct.transform(clean_train_df)
+        
+        new_columns = ct.get_feature_names_out()
+        
+        scaled_test_df = pd.DataFrame(scaled_clean_test, columns=new_columns)
+        scaled_train_df = pd.DataFrame(scaled_clean_train, columns=new_columns)
+        
+        scaled_test_df.to_csv('data/processed/scaled_clean_test.csv', index=False)
+        scaled_train_df.to_csv('data/processed/scaled_clean_train.csv', index=False)
+        
+        click.echo(click.style(f"Successfully saved scaled data", fg='green'))
+        
+        
+
         
 
     except Exception as e:
